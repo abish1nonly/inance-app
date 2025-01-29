6f156320877d86b407847b45363cd10e7cb271df
abish1nonly <abisanthadchanamoorthy@yahoo.com>
2025-01-29 08:51:00 +0000

    commit

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,7 @@ import { Link,useParams, useNavigate } from "react-router-dom";
 import '../assets/js/custom';
 import { useEffect, useState } from "react";
 import { loadGapi } from "../assets/js/gapiConfig";
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel';
+import OwlCarousel from 'react-owl-carousel';
 
 
 const Home = () => {
@@ -13,6 +12,30 @@ const Home = () => {
     const { lang } = useParams();
     const [language, setLanguage] = useState(lang || "en");
     const [isExpanded, setIsExpanded] = useState(false);
+    const options = {
+        responsive: {
+          0: {
+            items: 1,
+          },
+          400: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          700: {
+            items: 1,
+          },
+          800: {
+            items: 2,
+          },
+          1000: {
+            items: 3,
+          }
+        },
+        nav: false,
+        dots: true
+      }
     useEffect(() => {
         loadGapi();
         if (window.myMap) {
@@ -399,7 +422,38 @@ const Home = () => {
                 {texts.corouseltit}
             </h2>
         </div>
-        <div className="carousel-wrap layout_padding2-top ">
+        <OwlCarousel className='owl-theme section' loop margin={20} autoplay {...options}>
+
+            {/* 1 */}
+            <div className="review item">
+                <h3>24 hour up-time</h3>
+                <p>The API will always be available with zero latency and little to no down-time</p>
+                <div className="review-img">
+                <img src="/trust.svg" alt=""/>
+                </div>
+            </div>
+
+            {/* 2 */}
+            <div className="review item">
+                <h3>Global coverage</h3>
+                <p>View the latest live exchange rates for all the crypto-currencies available worldwide.</p>
+                <div className="review-img">
+                <img src="/globe.svg" alt="" />
+                </div>
+            </div>
+
+
+            {/* 3 */}
+            <div className="review item">
+                <h3>Great Documentation</h3>
+                <p>To assist developers, we have curated a world-className documentation for the API</p>
+                <div className="review-img">
+                <img src="/shield.svg" alt="" className="trust" />
+                </div>
+            </div>
+        </OwlCarousel>
+        <OwlCarousel className='owl-theme section' loop margin={20} autoplay {...options}>
+            <div className="carousel-wrap layout_padding2-top ">
             <div className="owl-carousel">
                 <div className="item">
                     <div className="box">
@@ -479,7 +533,7 @@ const Home = () => {
                             <p>
                             {texts.cli_mess}
                             </p>
-                    </div>
+                        </div>
                     </div>
                 </div>
                 <div className="item">
@@ -510,7 +564,8 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
+        </OwlCarousel>
         </div>
     </section>
     <section className="contact_section layout_padding">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App';
+import 'owl.carousel/dist/assets/owl.carousel.css'; 
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
